1d60b804e6c1224a1b2db0287869f81682e2b183
jmmarsde <jmmarsde@ncsu.edu>
2022-11-23 19:54:30 -0500

    Add Madison Thomas and Example Melika

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git worktree add ../_site built;
+
+bundle exec jekyll build -d ../_site;
+
+cd ../_site || exit;
+git add -A;
+git commit -m "Update site";
+git push;
+cd - || exit;
+
+git worktree remove ../_site;
